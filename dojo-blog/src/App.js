594e417fbbbdd04c6a39e41af736c30b87e265e6
Brian <wamwangimathenge@gmail.com>
2023-01-13 17:11:46 +0300

    Homepage Component

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
   // Dynamic Variable
@@ -14,7 +15,7 @@ function App() {
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <Home />
       </div>
     </div>
   );
