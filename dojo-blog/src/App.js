323f3dcf80aff395b9563a653a6dc1a9a2a0ce12
Brian <wamwangimathenge@gmail.com>
2023-01-13 16:56:24 +0300

    Dynamic Variables Added

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
 function App() {
+  // Dynamic Variable
+  const title = "Welcome to the new Blog"
+  const likes = 50;
+  // const person = { name: "Brian", age: 25 }
+  const link = "www.google.com"
+
+
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        
+        <p>{ "Hello Ninjas" }</p>
+        <p>{ [1, 2, 3, 4, 5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link }>Google</a>
       </div>
     </div>
   );
