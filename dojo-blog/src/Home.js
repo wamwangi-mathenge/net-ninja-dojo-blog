2c8319052318260ca1cdf0f4db38d2167bad54e2
Brian <wamwangimathenge@gmail.com>
2023-01-14 15:56:21 +0300

    onClick Events Added

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+const handleClick = () => {
+    console.log("Hello Ninjas")
+}
+
+const handleClickAgain = (name) => {
+    console.log("Hello " + name)
+}
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => {
+                handleClickAgain('Brian')
+            }}>Click Me Again</button>
         </div>
      );
 }
